37799d66623533aac7528c5b383e89d4acd70d7c
Harikrishna-Bommisetti <116732336+harikrishnaB18@users.noreply.github.com>
2025-02-10 23:55:51 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ test('renders learn react link', async () => {
   render(<App />);
 
   // Using findByText for async rendering
-  const linkElement = await screen.findByText(/Learn React/i);  // Use findByText
+  const linkElement = await screen.findByText(/learn react/i);  // Use findByText
 
   expect(linkElement).toBeInTheDocument();
 });
