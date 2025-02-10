c0ed15bc0ff6d6e328c56ae3ed2f262ee93a79fb
Harikrishna-Bommisetti <116732336+harikrishnaB18@users.noreply.github.com>
2025-02-10 23:51:20 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import App from './App';
 
 test('renders learn react link', async () => {
   render(<App />);
-  
+
   // Using findByText for async rendering
-const linkElement = screen.getByText(/Learn React/i);
-  
+  const linkElement = await screen.findByText(/Learn React/i);  // Use findByText
+
   expect(linkElement).toBeInTheDocument();
 });
