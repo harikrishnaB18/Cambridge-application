383725c5fd2eb143edcf7ef7d2c28fbabafa9eb2
Harikrishna-Bommisetti <116732336+harikrishnaB18@users.noreply.github.com>
2025-02-13 16:56:55 +0530

    Update SellingProperty.js

--- src/SellingProperty/SellingProperty.js
+++ src/SellingProperty/SellingProperty.js
@@ -415,8 +415,7 @@ return (
         open={toastOpen}
         autoHideDuration={4000}
         onClose={() => setToastOpen(false)}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      >
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
         <Alert onClose={() => setToastOpen(false)} severity="error" sx={{ width: '100%' }}>
           Please fill out all fields before proceeding.
         </Alert>
