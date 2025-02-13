b48abcc0ded96a25810739f1cb5af4803fa3e874
Harikrishna-Bommisetti <116732336+harikrishnaB18@users.noreply.github.com>
2025-02-13 17:01:56 +0530

    Update SellingProperty.js

--- src/SellingProperty/SellingProperty.js
+++ src/SellingProperty/SellingProperty.js
@@ -410,16 +410,19 @@ return (
           </table>
         </div>
       </div>
+    <>
+  <Snackbar
+    open={toastOpen}
+    autoHideDuration={4000}
+    onClose={() => setToastOpen(false)}
+    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+  >
+    <Alert onClose={() => setToastOpen(false)} severity="error" sx={{ width: '100%' }}>
+      Please fill out all fields before proceeding.
+    </Alert>
+  </Snackbar>
+</>
 
-      <Snackbar
-        open={toastOpen}
-        autoHideDuration={4000}
-        onClose={() => setToastOpen(false)}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-        <Alert onClose={() => setToastOpen(false)} severity="error" sx={{ width: '100%' }}>
-          Please fill out all fields before proceeding.
-        </Alert>
-      </Snackbar>
 
       <ToastContainer />
       <FooterHomeTwo />
